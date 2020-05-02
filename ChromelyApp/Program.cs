38950a4c89fc1b694cc54260b5d59d00a2803493
Thiago Tota <totaone@gmail.com>
2020-05-02 23:43:31 +0200

    initial commit

---
+++ ChromelyApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using Chromely;
+using Chromely.Core;
+using Chromely.Core.Configuration;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = DefaultConfiguration.CreateForRuntimePlatform();
+            config.StartUrl = "https://localhost:44341"; //Url from ReactApp project
+
+            AppBuilder
+           .Create()
+           .UseApp<ChromelyBasicApp>()
+           .UseConfiguration<IChromelyConfiguration>(config)
+           .Build()
+           .Run(args);
+        }
+    }
+}
---
+++ ElectronNETApp/Startup.cs
@@ -0,0 +1,53 @@
+using ElectronNET.API.Entities;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Electron.NET
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+
+            ElectronBootstrap();
+        }
+
+        public async void ElectronBootstrap()
+        {
+            var browserWindow = await ElectronNET.API.Electron.WindowManager.CreateWindowAsync(new BrowserWindowOptions
+            {
+                Width = 1152,
+                Height = 864,
+                Show = false
+            });
+
+            browserWindow.OnReadyToShow += () => browserWindow.Show();
+            browserWindow.LoadURL("http://localhost:3000/"); //Url from ReactApp project
+            browserWindow.SetTitle("Electron.NET");
+        }
+    }
+}
